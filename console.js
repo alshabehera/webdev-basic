bbc1562d3fa747680e4ea206d72e71fec04743a7
alsha <alshabehera7@gmail.com>
2023-03-27 15:30:25 +0530

    console

---
+++ console.js
@@ -0,0 +1,26 @@
+var you=prompt('what do you want?');
+document.write(you);
+document.getElementById('hyung').innerHTML="event"+14/7;
+let a=89;
+console.log(a)
+a="alsha"
+console.log(a)
+b="ghs"
+console.log(b)
+
+const item= {
+    "harry":true,
+    "shorav":false,
+    "alsha":87,
+    "priya":"asd"
+}
+item["suku"] = "shubham"
+console.log(item)
+console.log(item.alsha)
+
+
+let x="alsha"
+y="6"
+console.log(x+y)
+console.log(typeof x)
+
